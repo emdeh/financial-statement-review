6287372dc401f5e57338dd8452c9fc8bb72653a5
emdeh <72545829+emdeh@users.noreply.github.com>
2025-03-11 18:58:16 +1100

    fix missing if check in cosmosdb

--- infra/bicep/cognitive_services.bicep
+++ infra/bicep/cognitive_services.bicep
@@ -6,21 +6,27 @@ param cognitiveServicesAccountName string
 @description('Location for the Cognitive Services account')
 param location string = resourceGroup().location
 
-@description('SKU for the Cognitive Services account')
+@allowed([
+  'S0'
+])
 param skuName string = 'S0'
 
-@description('The kind of Cognitive Services account. For OCR, "ComputerVision" is typical.')
-param accountKind string = 'ComputerVision'
-
-resource cognitiveServices 'Microsoft.CognitiveServices/accounts@2022-12-01' = {
+resource cognitiveServices 'Microsoft.CognitiveServices/accounts@2023-05-01' = {
   name: cognitiveServicesAccountName
   location: location
+  identity: {
+    type: 'SystemAssigned'
+  }
   sku: {
     name: skuName
   }
-  kind: accountKind
+  kind: 'AIServices'
   properties: {
-    apiProperties: {}
+    publicNetworkAccess: 'Disabled'
+    networkAcls: {
+      defaultAction: 'Deny'
+    }
+    disableLocalAuth: true
   }
 }
 
--- infra/scripts/5_provision_cosmosdb.sh
+++ infra/scripts/5_provision_cosmosdb.sh
@@ -10,14 +10,21 @@ set -o allexport; source infra/.env; set +o allexport
 : "${AZURE_RESOURCE_GROUP:?Environment variable AZURE_RESOURCE_GROUP is required}"
 : "${AZURE_LOCATION:?Environment variable AZURE_LOCATION is required}"
 : "${AZURE_COSMOS_DB_ACCOUNT_NAME:?Environment variable AZURE_COSMOS_DB_ACCOUNT_NAME is required}"
-# Optionally, you can also add validations for the database and container names if you decide to make them configurable
 
-echo "🔹 Deploying Azure Cosmos DB Account \"$AZURE_COSMOS_DB_ACCOUNT_NAME\"..."
+echo "🔹 Checking if Cosmos DB \"$AZURE_COSMOS_DB_ACCOUNT_NAME\" exists in Resource Group \"$AZURE_RESOURCE_GROUP\"..."
 
-az deployment group create \
-    --resource-group "$AZURE_RESOURCE_GROUP" \
-    --template-file "$(pwd)/infra/bicep/cosmosdb.bicep" \
-    --parameters cosmosDbAccountName="$AZURE_COSMOS_DB_ACCOUNT_NAME" location="$AZURE_LOCATION"
+# Check if the Cosmos DB account exists
+if az cosmosdb show --name "$AZURE_COSMOS_DB_ACCOUNT_NAME" --resource-group "$AZURE_RESOURCE_GROUP" &>/dev/null; then
+    echo "✅ Cosmos DB Account \"$AZURE_COSMOS_DB_ACCOUNT_NAME\" already exists. Skipping creation."
+
+else
+    echo "🔹 Deploying Azure Cosmos DB Account \"$AZURE_COSMOS_DB_ACCOUNT_NAME\"..."
+
+    az deployment group create \
+        --resource-group "$AZURE_RESOURCE_GROUP" \
+        --template-file "$(pwd)/infra/bicep/cosmosdb.bicep" \
+        --parameters cosmosDbAccountName="$AZURE_COSMOS_DB_ACCOUNT_NAME" location="$AZURE_LOCATION"
+fi
 
 echo "✅ Cosmos DB deployment complete!"
 
