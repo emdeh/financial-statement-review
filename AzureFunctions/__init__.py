40607c9636ed6ad9d3494790350041874f67d5f1
emdeh <72545829+emdeh@users.noreply.github.com>
2025-03-14 18:09:41 +1100

    Step 1 - Initial Setup and Trigger Configuration done - need to test

--- AzureFunctions/__init__.py
+++ AzureFunctions/__init__.py
@@ -1,4 +1,17 @@
 """
-__init__.py
-Initialization code for the Azure Function.
+    __init__.py
+    Initialization code for the Azure Function.
 """
+import logging
+
+def main(myblob: bytes, name: str):
+    """
+    Main entry point for the Azure Function.
+    """
+    logging.info(
+        "Blob trigger function processed blob\nName: %s\nSize: %d bytes",
+         name, len(myblob))
+    # todo: Extract metadata, process the document, call OCR and ML endpoints,
+    #       and eventually store the results in the database.
+    
+    # You can access metadata via additional parameters if defined in function.json.
