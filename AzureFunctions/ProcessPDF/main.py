4c2c4ffa38cabca3f675bc5697d103c5383d9bcf
emdeh <72545829+emdeh@users.noreply.github.com>
2025-03-17 15:23:54 +1100

    draft ocrservice and ignoring local.settings.json

--- AzureFunctions/ProcessPDF/main.py
+++ AzureFunctions/ProcessPDF/main.py
@@ -12,6 +12,7 @@ import os
 import azure.functions as func
 from services.logger import Logger
 from services.tracer import AppTracer
+from services.ocr_service import OcrService, OcrServiceError
 
 # Initialise the JSON logger for this function
 logger = Logger.get_logger("ProcessPDF", json_format=True)
@@ -26,12 +27,13 @@ instrumentation_key = os.environ.get(
 tracer = AppTracer(instrumentation_key)
 
 # Dummy simulation functions for OCR and ML model
-def simulate_ocr(pdf_bytes):
-    """
-    Simulates OCR processing on a PDF document.
-    
-    """
-    return "Extracted text from PDF. This is a simulated OCR output."
+#def simulate_ocr(pdf_bytes):
+#    """
+#    Simulates OCR processing on a PDF document.
+#
+#    """
+#    return "Extracted text from PDF. This is a simulated OCR output."
+
 
 def simulate_ml_classification(text):
     """
@@ -62,23 +64,28 @@ def main(myblob: func.InputStream):
         # Simulate reading the blob content (PDF bytes)
         pdf_bytes = myblob.read()
 
-        # Simulate OCR extraction
-        extracted_text = simulate_ocr(pdf_bytes)
-        logger.info("OCR extraction complete", extra={"extracted_text": extracted_text})
+        # OCR extraction
+        try:
+            ocr_result = OcrService().extract_text(pdf_bytes)
+            logger.info("OCR extraction complete", extra={"extracted_text": ocr_result})
+        except OcrServiceError as e:
+            logger.error("Error extracting text from PDF", extra={"error": str(e)})
+            return
+            # Continue processing (e.g., parse text, send to ML, etc)
 
         # Simulate ML model classification
-        classification_result = simulate_ml_classification(extracted_text)
+        classification_result = simulate_ml_classification(ocr_result)
         logger.info(
             "ML classification complete",
             extra={"classification_result": classification_result})
 
         # Simulate writing the classification result to a database
-        cosmost_response = simulate_write_to_db({
+        cosmos_response = simulate_write_to_db({
             "blob_name": myblob.name,
             "classification_result": classification_result,
-            "extracted_text": extracted_text
+            "extracted_text": ocr_result
         })
-        logger.info("CosmosDB write simulated", extra=cosmost_response)
+        logger.info("CosmosDB write simulated", extra=cosmos_response)
 
         # Optionally, add more details to the span if needed.
         span.add_attribute("blob_name", myblob.name)
