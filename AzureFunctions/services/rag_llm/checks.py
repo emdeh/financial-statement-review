c1523440ee475e0fd1f4a86d36fd9ddcbbadc57c
emdeh <72545829+emdeh@users.noreply.github.com>
2025-05-05 12:25:26 +1000

    checks docstrings

--- AzureFunctions/services/rag_llm/checks.py
+++ AzureFunctions/services/rag_llm/checks.py
@@ -1,12 +1,44 @@
 """
-Module docstring.
+services/rag_llm/checks.py
+Module for defining RAG+LLM checks on documents.
+
+This module defines a list of checks to be performed on documents
+using the RAG+LLM approach. Each check is represented by a
+CheckDef object, which includes the check name, field name,
+question, query, and other parameters. The checks are used to
+determine the presence of specific financial statements
+in the documents, such as profit and loss statements,
+balance sheets, and cash flow statements.
+
+Classes:
+--------
+    CheckDef: A class representing a check definition for RAG+LLM checks.
+    CHECKS: A list of CheckDef objects representing the checks to be performed.
+
 """
 from dataclasses import dataclass
 
 @dataclass
 class CheckDef:
     """
-    Class docstring.
+    A class representing a check definition for RAG+LLM checks.
+    This class includes the check name, field name, question,
+    query, and other parameters.
+
+    Attributes:
+    ----------
+        name (str): The name of the check.
+        field_name (str): The field name in the Pydantic model.
+        question (str): The question to be asked for the check.
+        query (str): The query to be used for the check.
+        k (int): The number of results to retrieve (default is 3).
+        system_prompt (str, optional): The system prompt to be used for the check.
+    
+    Notes:
+    -----
+        Checks must much the DocumentResult model in db_models.py.
+        For example if you have `hasBalanceSheet` in the model, you musth have 
+        BalanceSheet in the checks.py.
     """
     name: str
     field_name: str
@@ -36,8 +68,3 @@ CHECKS = [
     ),
 ]
 
-"""
-Checks must much the DocumentResult model in db_models.py.
-For example if you have `hasBalanceSheet` in the model, you musth have 
-BalanceSheet in the checks.py.
-"""
